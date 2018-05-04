ebbf974e801eb66f79a33d206cafce7fadc5c85d
Michael <strongestavenger@Michaels-MacBook-Pro.local>
2018-05-04 15:55:57 +0300

    [chore] add linting settings

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ gulp.task('lint-js', function () {
   return gulp.src(['**/*.js'])
   // eslint() attaches the lint output to the "eslint" property
   // of the file object so it can be used by other modules.
+
+  .concat('wger/core/static/js/popper.js')
+
     .pipe(eslint())
     // eslint.format() outputs the lint results to the console.
     // Alternatively use eslint.formatEach() (see Docs).
