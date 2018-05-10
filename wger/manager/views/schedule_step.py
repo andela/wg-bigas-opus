180c67ec5cd7a2467733036b0c0c1ac36fcc6306
meshnesh <tonnie.nesh@gmail.com>
2018-05-11 00:51:37 +0300

    [Chore] Fix flake8 standard code

--- wger/manager/views/schedule_step.py
+++ wger/manager/views/schedule_step.py
@@ -21,7 +21,7 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy, ugettext as _
 from django.db import models
 from django import forms
-from django.forms import ModelForm, ModelChoiceField, Form, ChoiceField
+from django.forms import ModelForm, ModelChoiceField, ChoiceField
 from django.views.generic import (
     CreateView,
     DeleteView,
@@ -59,13 +59,23 @@ class StepCreateView(WgerFormMixin, CreateView, PermissionRequiredMixin):
         have we access to the current user
         '''
         class StepForm(ModelForm, forms.Form):
-            weeks = tuple((element, "{} weeks".format(element)) for element in range(1, 53)) # Loops through the number of weeks to display them on the site
-            workout = ModelChoiceField(queryset=Workout.objects.filter(user=self.request.user))
-            cycle = ChoiceField(choices=(("1", "Microcycle"), ("2", "Mesocycle"), ("3", "Macrocycle"), ("4", "Custom")), initial="4",
-                                widget=forms.Select(attrs={'onchange': 'cycleChange()'})) # cycles that a user can choose for the workout schedule
+            weeks = tuple(
+                (element, "{} weeks".format(element))
+                for element in range(1, 53)
+            )
 
-            duration = ChoiceField(choices=weeks, initial=1,
-                                   widget=forms.Select(),  help_text=_('The duration in weeks')) # duration of the weeks that a user can select
+            workout = ModelChoiceField(queryset=Workout.objects.filter(user=self.request.user))
+            cycle = ChoiceField(choices=(
+                ("1", "Microcycle"),
+                ("2", "Mesocycle"),
+                ("3", "Macrocycle"),
+                ("4", "Custom")),
+                initial="4",
+                widget=forms.Select(attrs={'onchange': 'cycleChange()'}))
+
+            duration = ChoiceField(
+                choices=weeks, initial=1,
+                widget=forms.Select(),  help_text=_('The duration in weeks'))
 
             class Meta:
                 model = ScheduleStep
