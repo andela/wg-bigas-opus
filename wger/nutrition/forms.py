95766fa0c298b8c74949a1866966433b70857dfa
Philip Kalela <PhilSkiiiwalker@Philips-MacBook-Pro.local>
2018-05-11 07:48:09 +0300

    [feature] Change how user adds meals and add a planned meals table

--- wger/nutrition/forms.py
+++ wger/nutrition/forms.py
@@ -23,6 +23,7 @@ from wger.core.models import UserProfile
 from wger.nutrition.models import (
     IngredientWeightUnit,
     Ingredient,
+    Meal,
     MealItem
 )
 from wger.utils.widgets import Html5NumberInput
@@ -123,6 +124,9 @@ class DailyCaloriesForm(forms.ModelForm):
 
 
 class MealItemForm(forms.ModelForm):
+    """
+     Form that takes meal item data from user
+    """
     weight_unit = forms.ModelChoiceField(queryset=IngredientWeightUnit
                                          .objects.none(),
                                          empty_label="g",
@@ -151,3 +155,43 @@ class MealItemForm(forms.ModelForm):
             self.fields['weight_unit'].queryset = \
                 IngredientWeightUnit.objects \
                 .filter(ingredient_id=ingredient_id)
+
+
+class MealForm(forms.ModelForm):
+    '''
+        Model form for the meal
+    '''
+
+    meal_choice = forms.ChoiceField(choices=MealItem.MealChoice)
+    amount = forms.DecimalField(
+        widget=Html5NumberInput
+    )
+    weight_unit = forms.ModelChoiceField(
+        queryset=IngredientWeightUnit.objects.none(),
+        empty_label="g",
+        required=False)
+    ingredient = forms.ModelChoiceField(
+        queryset=Ingredient.objects.all(),
+        widget=forms.HiddenInput)
+
+    class Meta:
+        model = Meal
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(MealForm, self).__init__(*args, **kwargs)
+
+        # Get the ingredient_id
+        ingredient_id = None
+
+        if kwargs.get('instance'):
+            ingredient_id = kwargs['instance'].ingredient_id
+
+        if kwargs.get('data'):
+            ingredient_id = kwargs['data']['ingredient']
+
+        # Filter the available ingredients
+        if ingredient_id:
+            self.fields['weight_unit'].queryset = \
+                IngredientWeightUnit.objects.filter(
+                    ingredient_id=ingredient_id)
