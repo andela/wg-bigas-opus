ca69e064e56b6f5b3c958c8fdfecc99777f4bb28
mutoromike <mikemutoro@gmail.com>
2018-05-13 12:18:35 +0300

    [fix] remove whitenoise configurations

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -107,8 +107,6 @@ BOWER_INSTALLED_APPS = (
 
 
 MIDDLEWARE_CLASSES = (
-    #whitenoise to enable adding static files
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
