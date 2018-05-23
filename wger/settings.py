582eed999d267f71ddd922d72e1e4a8b78f5058b
meshnesh <tonnie.nesh@gmail.com>
2018-05-23 14:09:40 +0300

    [Chore] Fix broken settings

--- wger/settings.py
+++ wger/settings.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from wger.settings_global import *  # noqa
+from wger.settings_global import *
 
 # Use 'DEBUG = True' to get more details for server errors
 DEBUG = True
-TEMPLATES[0]['OPTIONS']['debug'] = True  # noqa
+TEMPLATES[0]['OPTIONS']['debug'] = True
 
 ADMINS = (
     ('Your name', 'your_email@example.com'),
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/Users/shadowmaster/Desktop/Simulations/wger/wg-bigas-opus/database.sqlite',
+        'NAME': '/Volumes/Projects/simulations/wg-bigas-opus/wger/database.sqlite',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
@@ -25,7 +25,7 @@ DATABASES = {
 }
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'q1s551iop*h1=!rbjd%^%wra^*_fin247*9ss=t$sra3m&i7x^'
+SECRET_KEY = '8^=^s8^i(m!37s8dwwup_4(t7)9!w+^%3ycz9s7piq=q8ksfu+'
 
 # Your reCaptcha keys
 RECAPTCHA_PUBLIC_KEY = ''
@@ -39,21 +39,21 @@ SITE_URL = 'http://localhost:8000'
 
 # Path to uploaded files
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/Users/shadowmaster/.local/share/wger/media'
+MEDIA_ROOT = '/Users/antonyng/.local/share/wger/media'
 MEDIA_URL = '/media/'
 
 # Allow all hosts to access the application. Change if used in production.
 ALLOWED_HOSTS = '*'
 
 # This might be a good idea if you setup memcached
-# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+#SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
 # Configure a real backend in production
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Sender address used for sent emails
-WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'  # noqa
+WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'
 
 # Your twitter handle, if you have one for this instance.
-# WGER_SETTINGS['TWITTER'] = ''
+#WGER_SETTINGS['TWITTER'] = ''
