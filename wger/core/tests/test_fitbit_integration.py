0097f9720250c86331db956f48052d1f19918c5b
mutoromike <mikemutoro@gmail.com>
2018-05-24 12:01:25 +0300

    [tests] add fitbit tests

---
+++ wger/core/tests/test_fitbit_integration.py
@@ -0,0 +1,43 @@
+import os
+import urllib
+from django.core.urlresolvers import reverse
+from wger.core.tests.base_testcase import WorkoutManagerTestCase
+from django.conf import settings
+import requests
+import json
+from wger.core.views.fitbit import FitBit
+
+
+class IntegrateWithFitbitTestCase(WorkoutManagerTestCase):
+
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+    SCOPE = os.environ.get('SCOPE')
+    REDIRECT_URI = os.environ.get('REDIRECT_URI')
+    AUTHORIZE_URI = os.environ.get('AUTHORIZE_URI')
+
+    params = {
+            'client_id': CLIENT_ID,
+            'response_type': 'code',
+            'scope': SCOPE,
+            'redirect_uri': REDIRECT_URI
+    }
+
+    def test_it_shows_instructions(self):
+        # encode the parameters
+        urlparams = urllib.parse.urlencode(self.params)
+        # construct and return authorization_uri
+        url = self.AUTHORIZE_URI + '?' + urlparams
+        self.user_login()
+        r = self.client.get(reverse('core:fitbit-login'))
+        status_code = r.status_code
+        self.assertRedirects(r, url, status_code=302, target_status_code=200,
+        msg_prefix='', fetch_redirect_response=False)
+
+    def test_app_redirects_to_fitbit(self):
+        self.user_login()
+        r = self.client.get(reverse('core:fitbit-login'))
+        status_code = r.status_code
+        self.assertEqual(r.status_code, 302)
+
+   
