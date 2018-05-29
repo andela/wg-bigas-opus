d70798dc47aca46106ee336190ce9ec390c1dbd0
betsybeth <bethwambuimuniu@gmail.com>
2018-05-29 17:12:51 +0300

    chore:remove spaces

--- wger/core/views/user.py
+++ wger/core/views/user.py
@@ -82,6 +82,7 @@ def login(request):
                             template_name='user/login.html',
                             authentication_form=UserLoginForm,
                             extra_context=context)
+                            
 
 
 @login_required()
